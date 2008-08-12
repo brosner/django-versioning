9f2aaa5d2c30bbf8c714ec76dae5f55ac0e0015d
Brian Rosner <brosner@gmail.com>
2008-08-12 04:39:49 +0000

    Added the SHA1 to list_display in RevisionAdmin.
    git-svn-id: https://django-versioning.googlecode.com/svn/trunk@10 cdb1d5cb-5653-0410-9e46-1b5f511687a6

--- versioning/admin.py
+++ versioning/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from versioning.models import Revision
 
 class RevisionAdmin(admin.ModelAdmin):
-    list_display = ("content_type", "object_pk", "created_at")
+    list_display = ("sha1", "content_type", "object_pk", "created_at")
     list_filter = ("created_at", "content_type",)
 
 admin.site.register(Revision, RevisionAdmin)
